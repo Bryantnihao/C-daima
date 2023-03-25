3825ba4c0767e8de110b2f63ad39e6256a125e91
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-03-25 10:29:01 +0800

    Add files via upload

--- text.c
+++ text.c
@@ -1,16 +1,38 @@
-#include<stdoi.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
 int main()
-
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int k = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int ret = abc(arr, k, sz);
-	if (ret == -1)
+	printf("请输入年月日—>");
+	int  month, year, sum, lep,day;
+	scanf("%d %d %d", &year,&month,&day);
+
+	
+	switch (month)
 	{
-		printf("找不到指定的数字\n");
+	case 1:
+		sum = 0; break;
+	case 2:sum = 31; break;
+	case 3:sum = 59; break;
+	case 4:sum = 90; break;
+	case 5:sum = 120; break;
+	case 6:sum = 151; break;
+	case 7:sum = 181; break;
+	case 8:sum = 212; break;
+	case 9:sum = 243; break;
+	case 10:sum = 273; break;
+	case 11:sum = 304; break;
+	case 12:sum = 334; break;
+	default:printf("data error"); break;
+
 	}
-	else
-		printf("找到了，下标是：%d\n", ret);
-}   return 0;
+	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {// 判断是不是闰年
+		lep = 1;
+	}
+	else {
+		lep = 0;
+	}
+	if (lep == 1 && month>2) { // *如果是闰年且月份大于2,总天数应该加一天
+		sum++;
+	printf("这是今天的第%d天", sum);
+	return 0;
 }
