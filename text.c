d95571f7a97cba2dbaa7585d27fcb857654d682f
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-04-03 22:45:09 +0800

    有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。

--- text.c
+++ text.c
@@ -1,21 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
-{   
-	int a = 0;
-	int i = 0;
-	scanf("%d%d", &a, &i);
-	int t = a;
-	int sum = 0;
-      while(i > 0)
-	  {
-		  sum = sum + a;
-		  a = a * 10;
-		  a = a + t;
-		  
-		i--;
+{
+	double a = 2;
+	double b = 1;
+	double  sum = 0;
+	double i = 0;
+	for (i = 0; i < 20; i++)
+	{
+		sum = sum + a / b;
+		int t = 0;
+		t = a;
+		a = b + a;
+		b = t;
 	}
-	  printf("%d", sum);
-
+	printf("%lf\n", sum);
 	return 0;
 }
