501330f0663dd18a291e179eb20f2455da7f34c3
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-04-06 23:24:20 +0800

    int指针

--- text22.c
+++ text22.c
@@ -1,35 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int abc(int arr[], int k, int sz)
-{
-	int left = 0;
-	int right = sz - 1;
-	while (left <= right){
-		int mid = (left + right) / 2;
-		if (arr[mid] < k)
-		{
-			left = mid + 1;
-		}
-		else if (arr[mid]>k)
-		{
-			right = mid - 1;
-		}
-		else{
-			return mid;
-		}
-	} return -1;
-}
 int main()
-
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int k = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int ret = abc(arr, k, sz);
-	if (ret == -1)
-	{
-		printf("找不到指定的数字\n");
-	}
-	else
-		printf("找到了，下标是：%d\n", ret);
-}   
+	int a = 5;
+	int*b = &a;
+	int* *c = &b;
+	 **c = 10;
+	printf("%d %d %d", a, *b, **c);
+	printf("\n");
+	printf("%p %p %p", &a, b, *c);
+	return 0;
+}
