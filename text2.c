e00fc0f8c2bd7f39683092028e49387a7bfd06e8
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-05-08 21:55:17 +0800

    计算并输出200-400之间不能被3整除的整数的和 [200, 400)

--- text2.c
+++ text2.c
@@ -1,22 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<stddef.h>
-struct S
-{
-	char id[10];
-	char name[20];
-	int age;
-};
-void printf1(struct S* tmp)
-{
-	printf("%s %s %d",(*tmp).id,tmp->name,tmp->age );
-}
 int main()
-{     
-	struct S a = { "123", "nihao", 20 };
-	printf1(&a);
-	printf("\n");
-	printf("%d\n", offsetof(struct S, id));
-		printf("%d\n", offsetof(struct S, name));
-	printf("%d\n", offsetof(struct S,age));
+{
+	int i = 0;
+  	 int sum = 0;
+	for (i = 200; i < 400; i++)
+	{
+		if (i % 3 != 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return 0;
 }
