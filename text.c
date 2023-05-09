0cd16a31e50a39bd9d5dec91664fc1d9f3adb6ee
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-05-09 21:54:40 +0800

    输入5个数，求和并输出。要求编写求和的函数。

--- text.c
+++ text.c
@@ -1,22 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+int Sum()
+{    
+	int i = 0;
+	int a = 0;
+	int num = 0;
+	printf("请输入5个数:");
+	for (i = 0; i < 5; i++)
+	{ 
+		
+		scanf("%d", &a);
+		num += a;
+	}
+
+	return num;
+}
 int main()
 {  
-	int num = 0;
-	int sum = 0;
-	int sc = 0;
-	int i = 0;
-	printf("请打印十个整数:");
-	for (i = 0; i < 10; i++)
-	{
-		scanf("%d", &num);
-		if (num>0)
-		{
-			sc += 1;
-			sum += num;
-		}
-
-	}
-	printf("非负数的个数为%d,非负数的和为:%d\n", sc, sum);
+	    
+	int ad = Sum();
+	printf("5个数的和为:%d\n", ad);
 	return 0;
 }
