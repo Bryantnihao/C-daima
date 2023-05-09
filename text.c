68bffe7c46ad7059b3c10963a185636f272d130e
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-05-09 13:13:59 +0800

    从键盘输入10个数，统计非负数的个数，并计算非负数的和

--- text.c
+++ text.c
@@ -1,16 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
-{   
+{  
+	int num = 0;
+	int sum = 0;
+	int sc = 0;
 	int i = 0;
-	int a = 0;
-	for (i = 0; i < 100; i++)
+	printf("请打印十个整数:");
+	for (i = 0; i < 10; i++)
 	{
-		if (i % 17 == 0)
+		scanf("%d", &num);
+		if (num>0)
 		{
-			a = i;
+			sc += 1;
+			sum += num;
 		}
+
 	}
-	printf("%d\n", a);
+	printf("非负数的个数为%d,非负数的和为:%d\n", sc, sum);
 	return 0;
 }
