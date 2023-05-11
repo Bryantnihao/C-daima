14561af561fe6793b7e8b2d569ecf33231fe6afd
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-05-11 23:21:53 +0800

    计算并输出1-200之间不能被5整除的整数的和。

--- text2.c
+++ text2.c
@@ -1,22 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
-{
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	for (a = 0; a < 10; a++)
+{   
+	int sum = 0;
+	int i = 0;
+	for (i = 0; i < 200; i++)
 	{
-		for (b = 0; b < 10; b++)
+		if (i % 5 != 0)
 		{
-			for (c = 0; c < 10; c++)
-			{
-				if (a * 100 + b * 10 + c + c * 100 + b * 10 + a == 1333)
-				{
-					printf("a=%d,b=%d,c=%d\n", a, b, c);
-				}
-			}
+			sum +=i;
 		}
 	}
+	printf("%d\n", sum);
 	return 0;
 }
+
