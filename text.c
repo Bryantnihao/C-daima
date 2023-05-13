a4ea3782f42d3188a6d8e5f04f2b308abe35c99d
Bryantnihao <127756722+Bryantnihao@users.noreply.github.com>
2023-05-13 13:19:17 +0800

    输入一串字符，将其中的大写字母变成对应的小写字母并输出。

--- text.c
+++ text.c
@@ -1,25 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+
 int main()
 {
-	int i = 0;
-	int j = 0;
-	char arr[100];
-	scanf("%s", &arr);
-	printf("原来数组%s\n", arr);
-	int   len = (int)strlen(arr);
-	for (i = 0; i < len; i++)
+	char a[100];
+	printf("输出一串字符串");
+	scanf("%s", a);
+	int len = (int)strlen(a);
+	for (int i = 0; i < len; i++)
 	{
-		if (arr[i] != 'a')
-		{
-			arr[j] = arr[i];
-			j++;
+		if (a[i] >= 'A' && a[i] <= 'Z') {
+			a[i] = a[i] + 32;
 		}
 	}
-	arr[j] = '\0';
-	printf("消除a的数组%s\n", arr);
-	
+		printf("%s", a);
+
 	return 0;
 }
 
+
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	char a[100];
+//	printf("输出一串字符串");
+//	scanf("%s", a);
+//	int len = (int)strlen(a);
+//
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (a[i] >= 'A' && a[i] <= 'Z') {
+//			a[i] = a[i] + 32;
+//		}
+//	}
+//
+//	printf("处理后的字符串为：%s\n", a);
+//
+//	return 0;
+//}
